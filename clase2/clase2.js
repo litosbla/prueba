4150cf072b90f91d4184ae3ceac7b8b6a2ac35bc
litosbla <carlos.blancoherrera@gmail.com>
2023-10-24 17:41:39 -0500

    hola

---
+++ clase2/clase2.js
@@ -0,0 +1,28 @@
+/*
+
+Usar Upper Camel Case
+
+*/
+class SerHumano{
+    constructor(nombre, genero,sexo,edad){
+        this.nombre=nombre
+        this.genero=genero
+        this.sexo=sexo
+        this.edad=edad
+    }
+    niNombreEs(){
+        return `holaaa, mi nombre es ${this.nombre}`
+    }
+}
+
+const unObjeto ={
+    nombre:'Jhonathan',
+    email:'carlostos@gmail.com'
+}
+/*
+object = {}
+array = []
+function (){}
+class{}
+etc
+*/
